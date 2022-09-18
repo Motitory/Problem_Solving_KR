66962414f729802130878149637e7afe4cf17daf
Motitory <78538708+Motitory@users.noreply.github.com>
2022-09-18 15:53:04 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argn[]) {
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j < N - i + 1; j++) {
+			cout << " ";
+		}
+		for (int j = 1; j <= 2 * i - 1; j++) {
+			if (j % 2 == 1) cout << "*";
+			else cout << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
