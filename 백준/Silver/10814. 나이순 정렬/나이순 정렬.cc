245fd2af6d3cfe653286ce6d29dda7656ff49018
Motitory <78538708+Motitory@users.noreply.github.com>
2022-09-29 10:37:08 +0900

    [Silver V] Title: 나이순 정렬, Time: 72 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool compare(pair<int, string> a, pair<int, string> b) {
+	return a.first < b.first;
+}
+
+
+int main(int argc, char* argn[]) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector<pair<int, string>> v;
+	int age; string name;
+	for (int i = 1; i <= N; i++) {
+		cin >> age >> name;
+		v.push_back(make_pair(age, name));
+	}
+
+	stable_sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < N; i++) {
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+
+	return 0;
+}
