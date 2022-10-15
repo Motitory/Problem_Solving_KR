c9f9e05ae06c1c5e5dad2ffe5482d252909b7715
Motitory <78538708+Motitory@users.noreply.github.com>
2022-10-15 19:23:55 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -11,17 +11,19 @@ int main(int argc, char* argn[]) {
 
 	int n, k;
 	cin >> n >> k;
-
 	d[0][0] = 1LL;
+	// d[k][n] = d[k - 1][n] + d[k][n - 1];
 	for (int i = 1; i <= k; i++) {
 		for (int j = 0; j <= n; j++) {
-			for (int l = 0; l <= j; l++) {
-				d[i][j] += d[i - 1][j - l];
-				d[i][j] %= mod;
-			}
+			// d[k - 1][n]
+			d[i][j] = d[i - 1][j];
+			// d[k][n - 1]
+			if (j - 1 >= 0)
+				d[i][j] += d[i][j - 1];
+			d[i][j] %= mod;
 		}
 	}
-	
+
 	cout << d[k][n] << '\n';
 
 	return 0;
