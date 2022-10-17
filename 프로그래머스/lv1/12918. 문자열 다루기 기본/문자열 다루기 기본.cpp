415705ad7aa080a078f6440f45a6112aaa09a4ac
Motitory <78538708+Motitory@users.noreply.github.com>
2022-10-17 19:47:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 3.58 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if (s.length() != 4 && s.length() != 6) return false;
+    for (auto c : s) {
+        if (c - '0' >= 0 && c - '0' <= 9) continue;
+        else {
+            answer = false;
+            break;
+        }
+    }
+    return answer;
+}
