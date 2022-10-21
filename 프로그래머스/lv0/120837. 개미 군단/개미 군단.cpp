00c79abb13228cd4af35877e409809b5caa5bf3d
Motitory <78538708+Motitory@users.noreply.github.com>
2022-10-21 12:53:41 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    if (hp / 5 >= 1) {
+        answer += hp / 5;
+        hp %= 5;
+    }
+    if (hp / 3 >= 1) {
+        answer += hp / 3;
+        hp %= 3;
+    }
+    answer += hp;
+    return answer;
+}
