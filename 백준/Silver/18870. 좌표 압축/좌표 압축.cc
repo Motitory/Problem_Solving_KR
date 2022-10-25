d2dcd8d7d6eff89fab2ac3169f31a5c11a369988
Motitory <78538708+Motitory@users.noreply.github.com>
2022-10-25 16:47:20 +0900

    [Silver II] Title: 좌표 압축, Time: 476 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argn[]) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	// 입력
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	// 복사
+	vector<int> vt = v;
+	// 정렬
+	sort(vt.begin(), vt.end());
+	// 중복 값 제거
+	vt.erase(unique(vt.begin(), vt.end()), vt.end());
+
+	for (int i = 0; i < n; i++) {
+		// i번째 요소 값 it
+		auto it = lower_bound(vt.begin(), vt.end(), v[i]);
+		cout << it - vt.begin() << ' ';
+	}
+		
+	return 0;
+}
