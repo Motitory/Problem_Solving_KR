f1692c78d0058430c4dd69aabd1f54f84ef8079e
Motitory <78538708+Motitory@users.noreply.github.com>
2022-11-02 19:50:06 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[10], num[10], c[10];
+
+void go(int index, int n, int m) {
+	if (index == m) {
+		for (int i = 0; i < m; i++) {
+			cout << num[a[i]];
+			if (i != m - 1) cout << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < n; i++)  {
+		if (c[i]) continue;
+		c[i] = true;
+		a[index] = i;
+		go (index + 1, n, m);
+		c[i] = false;
+	}
+
+}
+
+int main(int argc, char* argn[]) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> num[i];
+	sort(num, num + n);
+	go(0, n, m);
+
+	return 0;
+}
