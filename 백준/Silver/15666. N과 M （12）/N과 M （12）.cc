77272ee6928a4ab5130cca5236f4f20f86a71ec8
Motitory <78538708+Motitory@users.noreply.github.com>
2022-11-10 23:05:00 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2584 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int a[10], num[10], c[10];
+vector<vector<int>> d;
+
+void go(int index, int start, int n, int m) {
+	if (index == m) {
+		vector<int> temp;
+		for (int i = 0; i < m; i++)
+			temp.push_back(num[a[i]]);
+		d.push_back(temp);
+		return;
+	}
+	for (int i = start; i < n; i++) {
+		c[i] = true;
+		a[index] = i;
+		go(index + 1, i, n, m);
+		c[i] = false;
+	}
+}
+
+int main(int argc, char* argn[]) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)		
+		cin >> num[i];
+	sort(num, num + n);
+
+	go(0, 0, n, m);
+
+	sort(d.begin(), d.end());
+	d.erase(unique(d.begin(), d.end()), d.end());
+
+	for (auto &v : d) {
+		for (int i = 0; i < m; i++) {
+			cout << v[i];
+			if (i != m - 1) cout << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
