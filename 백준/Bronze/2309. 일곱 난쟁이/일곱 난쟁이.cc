8124183627e5d253ced85f461b60bb70816f7b23
Motitory <78538708+Motitory@users.noreply.github.com>
2023-01-05 20:10:01 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,40 +1,43 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(int argc, char* argn[]) {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+int arr[9];
+int sum;
+vector<int> v;
+pair<int, int> ret;
 
-	int arr[9], sum = 0;
+void solve() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < i; j++) {
+			if (sum - arr[i] - arr[j] == 100) {
+				ret = {i, j};
+				return;
+			}
+		}
+	}
+}
 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
 	for (int i = 0; i < 9; i++) {
 		cin >> arr[i];
 		sum += arr[i];
 	}
 
-	bool finish = false;
-	int result[7];
+	solve();
 
-	for (int i = 0; i < 8 && !finish; i++) {
-		for (int j = i + 1; j < 9 && !finish; j++) {
-			if (arr[i] + arr[j] == (sum - 100)) {
-				int cnt = 0;
-				for (int k = 0; k < 9; k++) {
-					if (k != i && k != j) {
-						result[cnt++] = arr[k];
-					}
-				}
-				finish = true;
-			}
-		}
+	for (int i = 0; i < 9; i++) {
+		if (ret.first == i || ret.second == i) continue;
+		v.push_back(arr[i]);
 	}
 
-	sort(result, result + 7);
+	sort(v.begin(), v.end());
 
-	for (int i = 0; i < 7; i++)
-		cout << result[i] << '\n';
+	for (int i : v) cout << i << '\n';
 
 	return 0;
 }
