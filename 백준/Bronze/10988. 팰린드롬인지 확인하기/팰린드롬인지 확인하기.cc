8004e2f3ea13b977c7f42f8b0c341d6e0af3e1f7
Motitory <78538708+Motitory@users.noreply.github.com>
2023-01-06 16:42:34 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -2,29 +2,14 @@
 
 using namespace std;
 
-string str;
-stack<char> stk;
-int ans = 1;
+string str, temp;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
 	cin >> str;
-
-	for (int i = 0; i < (str.length() + 1) / 2; i++) {
-		stk.push(str[i]);
-	}
-
-	for (int i = str.length() / 2; i < str.length(); i++) {
-		if (stk.top() == str[i]) stk.pop();
-		else {
-			ans = 0;
-			break;
-		}
-	}
-
-	cout << ans << '\n';
+	temp = str;
+	reverse(temp.begin(), temp.end());
+	if (temp == str) cout << 1 << '\n';
+	else cout << 0 << '\n';
 
 	return 0;
 }
