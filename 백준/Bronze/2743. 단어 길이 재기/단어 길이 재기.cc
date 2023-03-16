8104516d660c7da8ea1be6792ab506b230d21cd7
Motitory <78538708+Motitory@users.noreply.github.com>
2023-03-16 18:16:08 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main(int argc, char** argv) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> str;
+	cout << str.length() << '\n';
+
+	return 0;
+}
